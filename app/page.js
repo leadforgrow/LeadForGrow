43d6d4e352e462b76e86dbf829a9beba92aa954a
saurabh4323 <saurabhiitr01@gmail.com>
2026-02-05 03:18:10 +0530

    ee

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Script from "next/script";
 import UserHome from "./user/home/page";
 import LeadForGrowWidget from "./Enquiry";
 import AIChatPopup from "./components/AIChatPopup";
@@ -37,7 +38,43 @@ export default function page() {
           />
         </a>
       </div>
-      <AIChatPopup />
+      {/* <AIChatPopup /> */}
+      
+      {/* Interakt WhatsApp Widget */}
+      <Script
+        id="interakt-kiwi"
+        strategy="lazyOnload"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,c,r,a,m){
+              w['KiwiObject']=r;
+              w[r]=w[r] || function () {
+                (w[r].q=w[r].q||[]).push(arguments)};
+              w[r].l=1*new Date();
+                a=d.createElement(s);
+                m=d.getElementsByTagName(s)[0];
+              a.async=1;
+              a.src=c;
+              m.parentNode.insertBefore(a,m)
+            })(window,document,'script',"https://app.interakt.ai/kiwi-sdk/kiwi-sdk-17-prod-min.js?v="+ new Date().getTime(),'kiwi');
+            
+            const initKiwi = () => {
+                if (window.kiwi && typeof window.kiwi.init === 'function') {
+                    window.kiwi.init('', 'txl2ro13PPN8TfMgn4xMNKVy3D7ptcQA', {});
+                } else {
+                    // Script not fully loaded yet, retry in a moment
+                    setTimeout(initKiwi, 200);
+                }
+            };
+            
+            if (document.readyState === 'complete') {
+                initKiwi();
+            } else {
+                window.addEventListener("load", initKiwi);
+            }
+          `,
+        }}
+      />
     </div>
   );
 }
